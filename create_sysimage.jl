649937e6f478a00fdc378f3cac267c5d972e46c6
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2023-02-05 18:15:50 +0800

    cleanup

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,5 @@
 using PackageCompiler
 create_sysimage([:Pluto, :PlutoUI, :WordCloud, :HTTP, :ImageIO, :Images];
-                sysimage_path="sys_wordcloud.so",
+                sysimage_path = "sys_wordcloud.so",
                 precompile_execution_file = "warmup.jl",
                 cpu_target = PackageCompiler.default_app_cpu_target())
